0e20db134269ff436cbf50db750cb04ebee03564
Kevin Klukowski <kklukowski12@gmail.com>
2021-02-11 18:30:32 -0500

    finishing useDark hook, mvp complete

---
+++ src/hooks/useDark.js
@@ -0,0 +1,9 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = () => {
+    const [isDark, setIsDark] = useLocalStorage('darkMode', false);
+
+    return [isDark, setIsDark]
+}
+
+export default useDarkMode;
